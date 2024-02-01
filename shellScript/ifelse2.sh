0cd7e342fe2e55f975c1941a54626f61cb0e0095
kushaggarwal <kush.aggarwal110085@gmail.com>
2024-02-01 06:59:44 +0530

    add switch case files

---
+++ shellScript/ifelse2.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+m=20
+n=30
+
+
+if [ $n -ge $m ]
+then 
+	echo "N is greated than M"
+else 
+	echo "M is greater than N"
+fi
---
+++ shellScript/switchcase.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+echo "Please choose name of the operating system"
+read name
+
+case $name in
+	"Linux") echo "The option chosen is Linux";;
+	"Widnows") echo "The option chosen is Windows";;
+	"Macos") echo "The option chosen is macos";;
+	"*") echo "Invalid Input"
+esac
