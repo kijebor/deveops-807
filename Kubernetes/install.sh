a72a9f12baade7aeb995add5969ac1a9ae3e1efa
kushaggarwal <kush.aggarwal110085@gmail.com>
2024-03-21 06:06:14 +0530

    Script for minikube

--- Kubernetes/install.sh
+++ Kubernetes/install.sh
@@ -1,5 +1,9 @@
-#!/bin/zsh
-
+#!/bin/bash
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $USER
 newgrp docker
