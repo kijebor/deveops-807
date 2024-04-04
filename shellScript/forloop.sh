0f4e88d641f26d17e89a399d1e4e567a3b0b89c4
kushaggarwal <kush.aggarwal110085@gmail.com>
2024-04-04 06:06:13 +0530

    Update manifest

--- shellScript/forloop.sh
+++ shellScript/forloop.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-for i in 10 20 35 40 58
+for i in 88 79 6 56 78 10 90
 do
-	echo The current iteration is $i
+	# echo The current iteration is $i
 	if [ $i -eq 10 ]
 	then
 		echo "We found the number 10"
