e52e6d4dab84328370f6f3d95a415a8f5d323ceb
kushaggarwal <kush.aggarwal110085@gmail.com>
2024-04-23 06:17:10 +0530

    ansible

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,30 +1,30 @@
 # Create normal EC2 instance resource using resource
 
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
+#   owners = ["099720109477"] # Canonical
+# }
 
-resource "aws_instance" "web1" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+# resource "aws_instance" "web1" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = var.instance_type
 
-  tags = {
-    Name = "HelloWorld54"
-  }
-}
+#   tags = {
+#     Name = "HelloWorld54"
+#   }
+# }
 
 # resource "aws_s3_bucket" "sample_bucket" {
 
@@ -32,16 +32,39 @@ resource "aws_instance" "web1" {
   
 # }
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+# module "s3_bucket" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = var.bucket_name
-  acl    = "private"
+#   bucket = var.bucket_name
+#   acl    = "private"
 
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
+#   control_object_ownership = true
+#   object_ownership         = "ObjectWriter"
 
-  versioning = {
-    enabled = true
-  }
+#   versioning = {
+#     enabled = true
+#   }
+# }
+
+module "my_instance" {
+  source = "./modules/ec2-instance"
+  instance_type = "t3.small"
+  instance_name = "prod"
+}
+
+module "my_instance" {
+  source = "./modules/ec2-instance"
+  instance_type = "t3.small"
+  instance_name = "test"
+}
+
+module "my_instance" {
+  source = "./modules/ec2-instance"
+  instance_type = "t3.small"
+  instance_name = "dev"
+}
+
+module "my_bucket" {
+  source = "./modules/s3"
+  bucket_name = "test"
 }
